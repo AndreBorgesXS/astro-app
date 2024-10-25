289358a2b4835ba3f395069daf741e4771f0106c
andreborges.xs <andreborges.xs@gmail.com>
2024-10-25 18:44:52 +0000

    adapter cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 
 import { defineConfig } from 'astro/config';
-import react from
- '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+   import react from '@astrojs/react';
+   import tailwind from '@astrojs/tailwind';
+   import cloudflare from '@astrojs/cloudflare';
 
-export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: 'server',
-});
+   export default defineConfig({
+     integrations: [react(), tailwind()],
+     output: 'server',
+     adapter: cloudflare() // Add this line
+   });
