2ec4712ebdf3f6597569e1916aceb05e197f60d6
andreborges.xs <andreborges.xs@gmail.com>
2024-10-25 18:39:07 +0000

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
+
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from "@astrojs/tailwind";
+import react from
+ '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: 'server',
 });
